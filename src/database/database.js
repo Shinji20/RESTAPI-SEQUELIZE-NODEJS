29590d13f705726feb92cf877e960d15684e0579
taakax <ccastillosantiago668@gmail.com>
2023-11-28 10:20:05 -0500

    segundo

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,6 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize("projectsdb", "postgres", "12345678", {
+export const sequelize = new Sequelize("projectdb", "postgres", "12345678", {
   host: "localhost",
   dialect: "postgres",
 });
-  
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
 
-import "./models/Project.js"
+import "./models/Project.js";
+import "./models/Task.js";
 
 async function main() {
   try {
-    await sequelize.sync()
+    await sequelize.sync({ force: true });
     app.listen(4000);
     console.log("listening on port", 4000);
   } catch (error) {
