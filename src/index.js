fbd73b92025b8ff935b143280c8227c026f8844c
taakax <ccastillosantiago668@gmail.com>
2023-11-29 10:26:37 -0500

    crud de project terminado

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import app from "./app.js";
 import { sequelize } from "./database/database.js";
 
-import "./models/Project.js";
-import "./models/Task.js";
-
 async function main() {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
     app.listen(4000);
     console.log("listening on port", 4000);
   } catch (error) {
---
+++ src/routes/projects.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createProject, deleteProject, getProject, getProjects, updateProject } from "../controllers/projects.controller.js";
+
+const router = Router();
+
+router.get("/projects", getProjects);
+router.post("/projects", createProject);
+router.put("/projects/:id", updateProject);
+router.delete("/projects/:id", deleteProject);
+router.get("/projects/:id", getProject);
+
+export default router;
